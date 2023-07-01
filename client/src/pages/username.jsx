4e56cbafddd898fda9dc796009d65769af21ff49
Banja David Lotigo <innocdavid@gmail.com>
2023-07-01 23:51:47 +0300

    Feature: Handle form validation with formik

--- client/src/pages/username.jsx
+++ client/src/pages/username.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../assets/images/avatar.png'
-import styles from '../assets/styles/username.module.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { useFormik } from 'formik';
+import logo from '../assets/images/avatar.png';
+import { validateUsername } from '../Helper/validate';
+
+import styles from '../assets/styles/username.module.css';
 
 export default function Username() {
+
+  const formik = useFormik({
+    initialValues: {
+      username: ''
+    },
+    validate: validateUsername,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async values => { console.log(values); },
+  });
   return (
     <div className="container mx-auto">
+      <Toaster position='top-center' reverseOrder={false}></Toaster>
       <div className="flex justify-center items-center h-screen">
         <div className={styles.glass}>
           <div className="title flex flex-col items-center">
@@ -13,12 +28,12 @@ export default function Username() {
             <small className="py-2 text-xl w-5/6 text-center text-gray-500">
               Explore more by connecting with us.
             </small>
-            <form className='py-1'>
+            <form className='py-1' onSubmit={formik.handleSubmit}>
               <div className="profile flex justify-center py-4">
                 <img src={logo} alt="logo" className={styles.profile_img} />
               </div>
               <div className="textbot flex flex-col items-center gap-5">
-                <input  type="text" placeholder="Username" className={styles.input_field}/>
+                <input {...formik.getFieldProps('username')}  type="text" placeholder="Username" className={styles.input_field}/>
                 <button type="submit" className={styles.btn}>Lets Go</button>
               </div>
               <div className="text-center py-4">
