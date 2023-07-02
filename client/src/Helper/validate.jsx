15293993f7935372fb431ceae36f5ef8b0ecdb06
Banja David Lotigo <innocdavid@gmail.com>
2023-07-02 17:55:49 +0300

    Add function to validate and verify password

--- client/src/Helper/validate.jsx
+++ client/src/Helper/validate.jsx
@@ -5,6 +5,12 @@ export async function validateUsername(values) {
   return errors;
 }
 
+export async function validatePassword(values) {
+  const errors = verifyPassword({}, values);
+  return errors;
+}
+
+// validate username
 function verifyUsername(error = {}, values) {
   if (!values.username) {
     error.username = toast.error('Username Required...!');
@@ -13,5 +19,23 @@ function verifyUsername(error = {}, values) {
   } else if (values.username.length < 3) {
     error.username = toast.error('Invalid username...!');
   }
+  return error;
+}
+
+// validate password
+function verifyPassword(error = {}, values) {
+
+  const specialChars = /^(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/;
+
+  if (!values.password) {
+    error.password = toast.error('Password Required...!');
+  } else if (values.password.includes(' ')) {
+    error.password = toast.error('Invalid Password...!');
+  } else if (values.password.length < 4) {
+    error.password = toast.error('Password Too Short...!');
+  } else if (!specialChars.test(values.password)) {
+    error.password = toast.error('Password must contain at least one special character');
+  }
+
   return error;
 }
