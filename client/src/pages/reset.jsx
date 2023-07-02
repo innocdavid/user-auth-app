974aad8232d9bf0dda0cfc2fca3c245b92ac8da1
Banja David Lotigo <innocdavid@gmail.com>
2023-07-02 18:58:00 +0300

    Feature: Add a reset password component

---
+++ client/src/pages/reset.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { useFormik } from 'formik';
+import logo from '../assets/images/avatar.png';
+import { validateResetPassword } from '../helper/validate';
+
+import styles from '../assets/styles/username.module.css';
+
+export default function PasswordReset() {
+
+  const formik = useFormik({
+    initialValues: {
+      password: '',
+      confirm_password: '',
+    },
+    validate: validateResetPassword,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async values => { console.log(values); },
+  });
+  return (
+    <div className="container mx-auto">
+      <Toaster position='top-center' reverseOrder={false}></Toaster>
+      <div className="flex justify-center items-center mt-40">
+        <div className={styles.glass} style={{ width: "50"}}>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-4xl font-bold mt-2">Reset</h4>
+            <small className="py-2 text-xl w-5/6 text-center text-gray-500">
+              Enter new password.
+            </small>
+            <form className='pt-4' onSubmit={formik.handleSubmit}>
+              <div className="textbot flex flex-col items-center gap-5">
+                <input {...formik.getFieldProps('password')}  type="password" placeholder="New Password" className={styles.input_field}/>
+                <input {...formik.getFieldProps('confirm_password')}  type="password" placeholder="Repeat Password" className={styles.input_field}/>
+                <button type="submit" className={styles.btn}>Reset</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
