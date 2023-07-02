368c068787d76ac9af337c6f666d6ba0b3b29e86
Banja David Lotigo <innocdavid@gmail.com>
2023-07-02 18:37:37 +0300

    Feature: Add password recovery page

---
+++ client/src/pages/recovery.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { useFormik } from 'formik';
+import logo from '../assets/images/avatar.png';
+import { validatePassword } from '../helper/validate';
+
+import styles from '../assets/styles/username.module.css';
+
+export default function PasswordRecovery() {
+  return (
+    <div className="container mx-auto">
+      <Toaster position='top-center' reverseOrder={false}></Toaster>
+      <div className="flex justify-center items-center mt-40">
+        <div className={styles.glass}>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-4xl font-bold mt-2">Recovery</h4>
+            <small className="py-2 text-xl w-5/6 text-center text-gray-500">
+              Enter OTP to recover password.
+            </small>
+          </div>
+          <form className="pt-3">
+            <div className="textbox flex flex-col items-center gap-5">
+              <div className="input text-center">
+                <span className="py-2 text-sm text-left text-gray-500">
+                  Please enter the 6 digit OTP sent to your email address.
+                </span>
+                <div className="textbot flex flex-col items-center gap-5">
+                  <input type="text" placeholder="OTP" className={styles.input_field}/>
+                  <button type="submit" className={styles.btn}>Recover</button>
+                </div>
+              </div>
+              <div className="text-center py-4">
+                <span className="text-gay-500">
+                  Can't get OTP? <button className="text-sky-600">Resend</button>
+                </span>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
