b1e532e457613852b1c019587cf5f234651ae180
Banja David Lotigo <innocdavid@gmail.com>
2023-07-17 22:15:12 +0300

    Update the register app controller

--- server/controllers/appControllers.js
+++ server/controllers/appControllers.js
@@ -1,5 +1,40 @@
+import UserModel from '../models/user.js';
+
 export async function register(req, res) {
-  res.json('register route');
+  try {
+    const { username, password, profile, email } = req.body;
+    // check for existing username
+    const existingUsername = new Promise((resolve, reject) => {
+      UserModel.findOne({ username }, (err, user) => {
+        if (err) reject(new Error(err));
+        if (user) reject({ error: 'Username already taken' })
+
+        resolve();
+      });
+    });
+
+    Promise([existingEmail, existingEmail])
+      .then(() => {
+
+      }). catch(error => {
+        return res.status(500).json({
+          error: 'Enable hashed password'
+        })
+      })
+
+    // check for existing email address
+    const existingEmail = new Promise((resolve, reject) => {
+      UserModel.findOne({ email }, (err, email) => {
+        if (err) reject(new Error(err));
+        if (email) reject({ error: 'Email already taken' })
+
+        resolve();
+      });
+
+    });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
 }
 
 export async function login(req, res) {
