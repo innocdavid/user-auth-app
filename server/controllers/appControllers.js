937c296440b4f8877a9b2209eafb3e664b4059ef
Banja David Lotigo <innocdavid@gmail.com>
2023-07-18 21:49:19 +0300

    Add user login implementation on the controller

--- server/controllers/appControllers.js
+++ server/controllers/appControllers.js
@@ -1,5 +1,7 @@
 import UserModel from '../models/user.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import ENV from '../config.js'
 
 export async function register(req, res) {
   try {
@@ -35,10 +37,40 @@ export async function register(req, res) {
   }
 }
 
-
 export async function login(req, res) {
-  res.json('login route');
+  const { username, email, password } = req.body;
+
+  try {
+    let user;
+    if (email) {
+      user = await UserModel.findOne({ email });
+    } else if (username) {
+      user = await UserModel.findOne({ username });
+    }
+
+    if (!user) return res.status(400).json({ error: 'Username or email not found' });
+
+    const passwordCheck = await bcrypt.compare(password, user.password);
+    if (!passwordCheck) return res.status(403).json({ error: 'Invalid password' });
+
+    const token = jwt.sign({
+        userId: user._id,
+        username: user.username
+      },
+      ENV.JWT_SECRET,
+      { expiresIn: '24h' }
+    );
+
+    return res.status(200).json({
+      username: user.username,
+      token,
+      message: 'Login successful'
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 }
+
 
 export async function getUser(req, res) {
   res.json('get user');
